172e8d267b4a8e9bc8ec04482e09b20382b242d4
Leslie Castellanos <lesliekate@gmail.com>
2017-05-23 09:08:33 -0700

    Add log view

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 
     url(r'^garden/$', views.GardenView.as_view(), name='garden'),
+    url(r'^log/$', views.LogView.as_view(), name='log'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Garden, Location
+from .models import Garden, Log, Location
+
 
 class GardenView(generic.ListView):
     model = Garden
     template_name = "tracker/garden_list.html"
+
+class LogView(generic.ListView):
+    model = Log
+    template_name = "tracker/logs.html"
