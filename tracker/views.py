50b0943a3d78a1cf85b39be572a9a37bb095fa5a
Leslie Castellanos <lesliekate@gmail.com>
2017-06-04 14:37:36 -0700

    Show logs on landing page

--- tracker/views.py
+++ tracker/views.py
@@ -4,10 +4,24 @@ from django.views import generic
 from .models import Garden, Log, Location
 
 
-class GardenView(generic.ListView):
-    model = Garden
+class GardenView(generic.TemplateView):
+
     template_name = "tracker/garden_list.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(GardenView, self).get_context_data(**kwargs)
+        context['logs'] = Log.objects.all()
+        context['garden'] = Garden.objects.get(id=1)
+        return context
+
+
 class LogView(generic.ListView):
-    model = Log
+    # model = Log
     template_name = "tracker/logs.html"
+
+    queryset = Log.objects.order_by("-date")
+
+
+# class PlantView(generic.ListView):
+#     template_name = "tracker/plants.html"
+
