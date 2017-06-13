183c7290776a6a9e7dc6e8dded329bcdd308faba
leskat47 <leslie@eagerorange.com>
2017-06-13 08:43:15 -0700

    Note alternative for one to many relationship

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic.edit import CreateView
+# from django.http import UpdateForm
+import forms
 
 from .models import Garden, Log, Location, Planting, Plant
 
@@ -18,7 +21,7 @@ class PlantView(generic.ListView):
     """ Show list of current plant plantings and their status """
 
     template_name ='tracker/plant_list.html'
-
+    # queryset = Plant.objects.order_by("name").prefetch_related('planting_set')
     queryset = Plant.objects.order_by("name")
 
 
@@ -29,5 +32,7 @@ class LogView(generic.ListView):
     queryset = Log.objects.order_by("-date")
 
 
-# class PlantView(generic.ListView):
-#     template_name = "tracker/plants.html"
+class CreateLogView(CreateView):
+    model = Log
+    template_name = "tracker/update_form.html"
+    form_class = forms.LogForm
